1a9db2c98c3b5f4e4dcf1e6f5b34c606656e002e
Akemy Mory <akemymory@gmail.com>
2023-09-15 21:44:35 +0100

    Weather app updates

---
+++ index.js
@@ -0,0 +1,21 @@
+const apiKey = "8f1f02ca3a02460bcefe3eff78e9a6d6";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search buttom");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  var data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "ºc";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
