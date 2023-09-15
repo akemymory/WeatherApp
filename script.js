3c322570c5f0d18289362f33f3282b7ece375e2b
Akemy Mory <akemymory@gmail.com>
2023-09-15 21:55:46 +0100

    updates

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
+  document.querySelector(".error").style.display = "none";
+
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
